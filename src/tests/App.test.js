c02c0197a268fcaa87d5f0c62d6523c332a9a1c7
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-05-02 17:28:13 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,17 +1,80 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Router } from 'react-router-dom';
+import renderWithRouter from '../renderWithRouter';
 
 import App from '../App';
 
 describe('Testes App', () => {
-  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-    render(<App />);
+  it('Teste link página Home e path', () => {
+    const { history } = renderWithRouter(<App />);
 
     const firstLink = screen.getByRole('link', {
       name: /home/i,
     });
-    expect(firstLink(/Home/i)).toBeInTheDocument(); // https://stackoverflow.com/questions/58976251/checking-text-appears-inside-an-element-using-react-testing-library
+
+    expect(firstLink).toBeInTheDocument();
+    userEvent.click(firstLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Teste link página About e path', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const secondLink = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    expect(secondLink).toBeInTheDocument();
+    userEvent.click(secondLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Teste link página Favorite Pokémon e path', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const thirdLink = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+
+    expect(thirdLink).toBeInTheDocument();
+    userEvent.click(thirdLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Teste link página Favorite Pokémon e path', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const thirdLink = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+
+    expect(thirdLink).toBeInTheDocument();
+    userEvent.click(thirdLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Verifica o comportamento em caso de rota inexistente', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const invalidURL = '/henrique';
+
+    act(() => {
+      history.push(invalidURL);
+    });
+
+    const doNotFounded = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+
+    expect(doNotFounded).toBeInTheDocument();
   });
 });
