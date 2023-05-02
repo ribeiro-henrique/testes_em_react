7ad998499ff834f4a6bdbc0d4ef3ed464b893ea6
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-05-02 16:13:18 -0300

    req 1 parcialmente feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router-dom';
+
+import App from '../App';
+
+describe('Testes App', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    render(<App />);
+
+    const firstLink = screen.getByRole('link', {
+      name: /home/i,
+    });
+    expect(firstLink(/Home/i)).toBeInTheDocument(); // https://stackoverflow.com/questions/58976251/checking-text-appears-inside-an-element-using-react-testing-library
+  });
+});
