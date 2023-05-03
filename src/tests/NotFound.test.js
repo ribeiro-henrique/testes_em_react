c6d3821646984ebccc0a5af5c6e23834d505240e
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-05-03 10:32:56 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+import NotFound from '../pages/NotFound';
+
+describe('Testes FavoritePokemon', () => {
+  it('É exibida uma msg caso nenhum pokemon seja favoritado', () => {
+    renderWithRouter(<NotFound />);
+
+    const titleNotFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+
+    expect(titleNotFound).toBeInTheDocument();
+
+    const notFoundImg = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    expect(notFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
